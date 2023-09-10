9f9077b60086375aaba8c741c4814de81ade0e9a
georgmarts <118880240+georgmarts@users.noreply.github.com>
2023-09-10 18:23:58 +0500

    Delete server/api directory

--- server/api/posts.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from "../../lib/supabaseClient"
-
-export default defineEventHandler(async (event) => {
-    const { data } = await supabase.from('posts').select()
-    return {
-        data
-    }
-})

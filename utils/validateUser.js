ef0d36a84ec34b71f8605eb6fa440a274daafed4
georgmarts <118880240+georgmarts@users.noreply.github.com>
2023-09-10 18:22:30 +0500

    Update validateUser.js

--- utils/validateUser.js
+++ utils/validateUser.js
@@ -1,30 +1,13 @@
 import mysql from 'mysql2/promise'
-
-// type User = {
-//     id: number,
-//     firstName: string,
-//     lastName: string,
-//     email: string,
-//     phoneNumber: string
-// }
-
-// type Error = {
-//     error: string
-// }
+import { supabase } from '~/lib/supabaseClient';
 
 
 export async function validateUser(email, password) {
     try {
-        const connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'password',
-            database: 'blog'
-          });
-        
-        const [data] = await connection.execute(`SELECT * FROM users WHERE email = '${email}' and password = '${password}'`)
+        const data = supabase.from('users').select().eq('email', email).eq('password', password)
+        // console.log(data)
         return data
     } catch (error) {
-        return { error: 'Something went wrong, please try again'}
+        console.log("Server error")
     }
 }

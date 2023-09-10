8447cd641cfff1a5005e881a68fbeee4039a4cd3
georgmarts <118880240+georgmarts@users.noreply.github.com>
2023-09-10 11:08:57 +0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,23 @@ export default {
   nitro: {
     preset: 'vercel-edge',
   },
+  modules: [
+        '@pinia/nuxt',
+        '@sidebase/nuxt-auth',
+        '@nuxtjs/supabase',
+    ],
+    auth: {
+        origin: process.env.AUTH_ORIGIN
+    },
+    supabase: {
+       url: process.env.SUPABASE_URL,
+       key: process.env.SUPABASE_KEY,
+       redirect: false,
+       redirectOptions: {
+            login: '/login',
+            callback: '/confirm',
+            exclude: [],
+       }
+    },
+    css: ['~/assets/scss/main.scss']
 };

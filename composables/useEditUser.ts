6bb4414caa17e46cf024cc3f9d78987310ac43d2
georgmarts <118880240+georgmarts@users.noreply.github.com>
2023-09-12 15:23:36 +0500

    Update useEditUser.ts

--- composables/useEditUser.ts
+++ composables/useEditUser.ts
@@ -1,9 +1,10 @@
 import { User } from "~/types"
 
 export default async function useAddPost(updatedUser: User) {
-    await $fetch('/api/user', {
+    const response = await $fetch('/api/user', {
       method: 'PATCH',
       body: updatedUser
     })
     refreshNuxtData('users')
+    return { response }
   }
